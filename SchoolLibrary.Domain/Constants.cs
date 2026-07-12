5f810e190880aedad15095fb57e6ee8eb7a001f7
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-07-12 14:47:33 +0100

    Create Resource configuration

---
+++ SchoolLibrary.Domain/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary.Domain
+{
+    public static class Constants
+    {
+        public const int TitleMaxLength = 200;
+        public const int DescriptionMaxLength = 4000;
+        public const int AuthorMaxLength = 200;
+        public const int FilePathMaxLength = 1000;
+        public const int ExternalUrlMaxLength = 2000;
+        public const int CoverImagePathMaxLength = 1000;
+    }
+}
---
+++ SchoolLibrary.Infrastructure/Data/Configurations/ResourceConfiguration.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolLibrary.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static SchoolLibrary.Domain.Constants;
+
+namespace SchoolLibrary.Infrastructure.Data.Configurations
+{
+    public class ResourceConfiguration : IEntityTypeConfiguration<Resource>
+    {
+        public void Configure(EntityTypeBuilder<Resource> builder)
+        {
+            builder.HasKey(r => r.Id);
+
+            builder
+                .Property(r => r.Title)
+                .IsRequired()
+                .HasMaxLength(TitleMaxLength);
+
+            builder
+                .Property(r => r.Description)
+                .IsRequired()
+                .HasMaxLength(DescriptionMaxLength);
+
+            builder
+                .Property(r => r.Author)
+                .HasMaxLength(AuthorMaxLength);
+
+            builder
+                .Property(r => r.FilePath)
+                .HasMaxLength(FilePathMaxLength);
+
+            builder
+                .Property(r => r.ExternalUrl)
+                .HasMaxLength(2000);
+
+            builder
+                .Property(r => r.CoverImagePath)
+                .HasMaxLength(1000);
+
+            builder
+                .Property(r => r.Type)
+                .IsRequired();
+
+            builder
+                .Property(r => r.CreatedAtUtc)
+                .IsRequired();
+
+            builder
+                .HasOne(r => r.Subject)
+                .WithMany(s => s.Resources)
+                .HasForeignKey(r => r.SubjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(r => r.Category)
+                .WithMany(c => c.Resources)
+                .HasForeignKey(r => r.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
