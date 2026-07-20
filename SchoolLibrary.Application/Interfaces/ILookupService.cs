c556f179a15c3fbf4e74e9ba4d2a92b5de16ca34
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-07-20 22:39:32 +0100

    Create ILookupService

---
+++ SchoolLibrary.Application/Interfaces/ILookupService.cs
@@ -0,0 +1,21 @@
+﻿using SchoolLibrary.Application.DTOs.LookupDTOs;
+
+namespace SchoolLibrary.Application.Interfaces
+{
+    public interface ILookupService
+    {
+        Task<IReadOnlyCollection<SubjectLookupDto>>
+            GetSubjectsAsync(CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyCollection<CategoryLookupDto>>
+            GetCategoriesAsync(CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyCollection<GradeLevelLookupDto>>
+            GetGradeLevelsAsync(CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyCollection<SchoolClassLookupDto>>
+            GetSchoolClassesAsync(
+                int? gradeLevelId = null,
+                CancellationToken cancellationToken = default);
+    }
+}
