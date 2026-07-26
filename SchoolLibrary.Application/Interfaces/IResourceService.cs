9591c75a0096c1efc3ba8a9d13d82f45e110f2b6
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-07-26 18:22:46 +0100

    Create IResourceService

--- SchoolLibrary.Application/Interfaces/IResourceService.cs
+++ SchoolLibrary.Application/Interfaces/IResourceService.cs
@@ -1,6 +1,27 @@
-﻿namespace SchoolLibrary.Application.Interfaces
+﻿using SchoolLibrary.Application.DTOs.ResourceDTOs;
+
+namespace SchoolLibrary.Application.Interfaces
 {
     public interface IResourceService
     {
+        Task<IReadOnlyCollection<ResourceListDto>> GetAllAsync(
+        CancellationToken cancellationToken = default);
+
+        Task<ResourceDetailsDto?> GetByIdAsync(
+            Guid id,
+            CancellationToken cancellationToken = default);
+
+        Task<Guid> CreateAsync(
+            CreateResourceDto model,
+            CancellationToken cancellationToken = default);
+
+        Task<bool> UpdateAsync(
+            Guid id,
+            UpdateResourceDto model,
+            CancellationToken cancellationToken = default);
+
+        Task<bool> DeleteAsync(
+            Guid id,
+            CancellationToken cancellationToken = default);
     }
 }
