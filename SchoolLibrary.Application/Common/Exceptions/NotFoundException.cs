5e73fd2ae52b59ffeaca750f729980befde40516
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-07-31 21:28:13 +0100

    Create custom exceptions

---
+++ SchoolLibrary.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace SchoolLibrary.Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message)
+        : base(message)
+        {
+        }
+
+        public NotFoundException(string entityName, object key)
+            : base($"{entityName} с идентификатор '{key}' не беше намерен.")
+        {
+        }
+    }
+}
---
+++ SchoolLibrary.Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿namespace SchoolLibrary.Application.Common.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message)
+        : base(message)
+        {
+        }
+
+        public ValidationException(
+            string message,
+            IDictionary<string, string[]> errors)
+            : base(message)
+        {
+            Errors = errors;
+        }
+
+        public IDictionary<string, string[]> Errors { get; }
+            = new Dictionary<string, string[]>();
+    }
+}
