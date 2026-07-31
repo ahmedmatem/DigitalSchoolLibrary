d88b1d08e3f43cd7defef7e8d44837b69f3b581e
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-07-31 20:55:09 +0100

    Update resource constants

--- SchoolLibrary.Domain/Constants/ResourceConstants.cs
+++ SchoolLibrary.Domain/Constants/ResourceConstants.cs
@@ -6,11 +6,16 @@ namespace SchoolLibrary.Domain.Constants
 {
     public static class ResourceConstants
     {
+        public const int TitleMinLength = 3;
         public const int TitleMaxLength = 200;
+
+        public const int DescriptionMinLength = 10;
         public const int DescriptionMaxLength = 4000;
+
         public const int AuthorMaxLength = 200;
-        public const int FilePathMaxLength = 1000;
+
+        public const int FilePathMaxLength = 500;
         public const int ExternalUrlMaxLength = 2000;
-        public const int CoverImagePathMaxLength = 1000;
+        public const int CoverImagePathMaxLength = 500;
     }
 }
