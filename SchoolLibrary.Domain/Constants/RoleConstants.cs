a08aa4ac3ae768e126a0658ca83159a0ac6071f5
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-02 11:14:52 +0100

    Create Role constants

---
+++ SchoolLibrary.Domain/Constants/RoleConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolLibrary.Domain.Constants
+{
+    public static class RoleConstants
+    {
+        public const string Student = "Student";
+        public const string Teacher = "Teacher";
+        public const string Admin = "Admin";
+    }
+}
