61fbd82dd7d69130edb7bdba819002566bf2ff22
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-02 15:42:44 +0100

    Create CurrentUserService

---
+++ SchoolLibrary.Application/Common/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolLibrary.Application.Common.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        bool IsAuthenticated { get; }
+
+        Guid? UserId { get; }
+
+        bool IsInRole(string role);
+    }
+}
---
+++ SchoolLibrary.Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using SchoolLibrary.Application.Common.Interfaces;
+using System.Security.Claims;
+
+namespace SchoolLibrary.Infrastructure.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        private ClaimsPrincipal? User => httpContextAccessor.HttpContext?.User;
+
+        public bool IsAuthenticated => User?.Identity?.IsAuthenticated == true;
+
+        public Guid? UserId
+        {
+            get
+            {
+                var userIdValue = User?.FindFirstValue(
+                    ClaimTypes.NameIdentifier);
+
+                return Guid.TryParse(userIdValue, out var userId)
+                    ? userId
+                    : null;
+            }
+        }
+
+        public bool IsInRole(string role)
+        {
+            return User?.IsInRole(role) == true;
+        }
+    }
+}
