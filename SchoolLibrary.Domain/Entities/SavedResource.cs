0dd1a4e2649786ca311dfed23775953effecff37
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-04 20:39:36 +0100

    Add SavedResource entity

---
+++ SchoolLibrary.Domain/Entities/SavedResource.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolLibrary.Domain.Entities
+{
+    public class SavedResource
+    {
+        public Guid UserId { get; set; }
+
+        public Guid ResourceId { get; set; }
+
+        public DateTime SavedAtUtc { get; set; }
+
+        public Resource Resource { get; set; } = null!;
+    }
+}
