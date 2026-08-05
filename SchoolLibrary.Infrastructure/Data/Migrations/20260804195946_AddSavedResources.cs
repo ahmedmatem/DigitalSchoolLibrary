9e9d568b46281f734aaeac1140854932adad8f18
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-05 18:55:35 +0100

    Add SavedResource migration

---
+++ SchoolLibrary.Infrastructure/Data/Migrations/20260804195946_AddSavedResources.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolLibrary.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSavedResources : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SavedResources",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ResourceId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SavedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavedResources", x => new { x.UserId, x.ResourceId });
+                    table.ForeignKey(
+                        name: "FK_SavedResources_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SavedResources_Resources_ResourceId",
+                        column: x => x.ResourceId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedResources_ResourceId",
+                table: "SavedResources",
+                column: "ResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedResources_SavedAtUtc",
+                table: "SavedResources",
+                column: "SavedAtUtc");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SavedResources_UserId",
+                table: "SavedResources",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SavedResources");
+        }
+    }
+}
