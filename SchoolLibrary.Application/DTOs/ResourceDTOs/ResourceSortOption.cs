38e12c398c83205c34a6d8d781f021c1d0521f12
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-08-25 20:01:34 +0100

    Add ResourceSortOptions

---
+++ SchoolLibrary.Application/DTOs/ResourceDTOs/ResourceSortOption.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolLibrary.Application.DTOs.ResourceDTOs
+{
+    public enum ResourceSortOption
+    {
+        Newest = 1,
+        Oldest = 2,
+        TitleAscending = 3,
+        TitleDescending = 4
+    }
+}
