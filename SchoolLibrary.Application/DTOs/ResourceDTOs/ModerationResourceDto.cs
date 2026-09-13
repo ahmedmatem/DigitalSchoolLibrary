1570f5b12b1fc8c1be8e91b5c698a9e5dc79c8c6
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2026-09-13 17:36:26 +0300

    Expose resource collection in resource DTO

--- SchoolLibrary.Application/DTOs/ResourceDTOs/ModerationResourceDto.cs
+++ SchoolLibrary.Application/DTOs/ResourceDTOs/ModerationResourceDto.cs
@@ -1,4 +1,4 @@
-﻿using SchoolLibrary.Domain.Enums;
+using SchoolLibrary.Domain.Enums;
 
 namespace SchoolLibrary.Application.DTOs.ResourceDTOs
 {
@@ -12,9 +12,11 @@ namespace SchoolLibrary.Application.DTOs.ResourceDTOs
 
         public string? Author { get; set; }
 
+        public ResourceCollectionType CollectionType { get; set; }
+
         public ResourceType Type { get; set; }
 
-        public string SubjectName { get; set; } = string.Empty;
+        public string? SubjectName { get; set; }
 
         public string CategoryName { get; set; } = string.Empty;
 
